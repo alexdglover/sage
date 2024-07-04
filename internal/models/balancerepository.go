60cd7d6c6b57e17e4f37c12dad4bcf5566ae9e0e
Alex Glover <alexdglover@gmail.com>
2024-07-04 14:43:48 -0500

    fix: use gorm methods for subqueries

--- internal/models/balancerepository.go
+++ internal/models/balancerepository.go
@@ -25,63 +25,23 @@ func (br BalanceRepository) GetAllBalances(ctx context.Context) ([]Balance, erro
 }
 
 func (br BalanceRepository) GetBalancesOfAllAssets(ctx context.Context, startYearMonth string, endYearMonth string) []Balance {
-	// TODO: Replace this with something more idiomatic to GORM if possible
 	var result []Balance
-	db.Raw(`SELECT *
-		FROM balances
-		WHERE account_id in (
-			SELECT id
-			FROM accounts
-			WHERE account_type="asset"
-		)
-		AND effective_start_date >= ?
-		AND (effective_end_date < ? or effective_end_date is null)`,
-		startYearMonth,
-		endYearMonth,
-	).Scan(&result)
+	assetAccountIds := db.Select("id").Where("account_type=?", "asset").Table("accounts")
+	db.Select("*").
+		Where("account_id IN (?)", assetAccountIds).
+		Where("effective_start_date >= ?", startYearMonth).
+		Where(db.Where("effective_end_date < ?", endYearMonth).Or("effective_end_date IS NULl")).Find(&result)
+
 	return result
 }
 
 func (br BalanceRepository) GetBalancesOfAllLiabilities(ctx context.Context, startYearMonth string, endYearMonth string) []Balance {
-	// TODO: Replace this with something more idiomatic to GORM if possible
 	var result []Balance
-	db.Raw(`SELECT *
-		FROM balances
-		WHERE account_id in (
-			SELECT id
-			FROM accounts
-			WHERE account_type="liability"
-		)
-		AND effective_start_date >= ?
-		AND (effective_end_date < ? or effective_end_date is null)`,
-		startYearMonth,
-		endYearMonth,
-	).Scan(&result)
+	liabilityAccountIds := db.Select("id").Where("account_type=?", "liability").Table("accounts")
+	db.Select("*").
+		Where("account_id IN (?)", liabilityAccountIds).
+		Where("effective_start_date >= ?", startYearMonth).
+		Where(db.Where("effective_end_date < ?", endYearMonth).Or("effective_end_date IS NULl")).Find(&result)
+
 	return result
 }
-
-// func (br BalanceRepository) getBalanceAllAssets(ctx context.Context, startDate string, endDate string) BalanceAmount {
-// 	// var balanceAmount float32
-// 	// var result BalanceAmount
-// 	db.Raw(`SELECT date, SUM(balance) as balance
-// 	FROM balances
-// 	WHERE
-// 		account_id in (SELECT id from accounts where AssetOrLiability='Asset')
-// 		AND effective_start_date >= ?
-// 		AND (effective_end_date < ? or effective_end_date is null)`,
-// 		startDate, endDate).Scan(&result)
-// 	return result
-// }
-
-// func (br BalanceRepository) getBalanceAllLiabilities(ctx context.Context, startDate string, endDate string) BalanceAmount {
-// 	// var balanceAmount float32
-// 	var result BalanceAmount
-// 	db.Raw(`SELECT date, SUM(balance) as balance
-// 	FROM balances
-// 	WHERE
-// 		account_id in (SELECT id from accounts where AssetOrLiability='Liability')
-// 		AND effective_start_date >= ?
-// 		AND (effective_end_date < ? or effective_end_date is null)`,
-// 		startDate, endDate).Scan(&result)
-// 	return result
-// }
