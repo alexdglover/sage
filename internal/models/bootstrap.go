0f49cd9cbbb53cc8dc24490c43f6b477f4d6b07e
Alex Glover <alexdglover@gmail.com>
2024-07-04 16:08:39 -0500

    minor: adds ui tweaks, more sample data, net worth table

--- internal/models/bootstrap.go
+++ internal/models/bootstrap.go
@@ -37,8 +37,19 @@ func BootstrapDatabase(ctx context.Context) {
 		db.Create(&Account{Name: "schmisa", AccountCategory: "credit card", AccountType: "liability"})
 
 		// Create balances
+		db.Create(&Balance{Date: "2024-02-13", EffectiveStartDate: "2024-02-13", Balance: 410.62, AccountId: 1})
+		db.Create(&Balance{Date: "2024-02-17", EffectiveStartDate: "2024-02-17", Balance: 173.87, AccountId: 2})
 		db.Create(&Balance{Date: "2024-03-13", EffectiveStartDate: "2024-03-13", Balance: 420.32, AccountId: 1})
 		db.Create(&Balance{Date: "2024-03-17", EffectiveStartDate: "2024-03-17", Balance: 103.87, AccountId: 2})
+		db.Create(&Balance{Date: "2024-04-13", EffectiveStartDate: "2024-04-13", Balance: 490.32, AccountId: 1})
+		db.Create(&Balance{Date: "2024-04-17", EffectiveStartDate: "2024-04-17", Balance: 133.12, AccountId: 2})
+		db.Create(&Balance{Date: "2024-04-13", EffectiveStartDate: "2024-05-13", Balance: 640.97, AccountId: 1})
+		db.Create(&Balance{Date: "2024-04-17", EffectiveStartDate: "2024-05-17", Balance: 140.44, AccountId: 2})
+		db.Create(&Balance{Date: "2024-04-13", EffectiveStartDate: "2024-06-13", Balance: 632.01, AccountId: 1})
+		db.Create(&Balance{Date: "2024-04-17", EffectiveStartDate: "2024-06-17", Balance: 132.55, AccountId: 2})
+		db.Create(&Balance{Date: "2024-04-13", EffectiveStartDate: "2024-07-13", Balance: 699.74, AccountId: 1})
+		db.Create(&Balance{Date: "2024-04-17", EffectiveStartDate: "2024-07-17", Balance: 102.76, AccountId: 2})
+
 	}
 
 }
