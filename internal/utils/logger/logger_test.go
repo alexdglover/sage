3cf089fd6a1d296645280a3f7301a9bec381db60
doug-benn <fiery.coder@gmail.com>
2025-02-15 13:39:02 +0000

    chore: Added Logger (#63)
    * Added customised multi output logger
    
    * logger basic test

---
+++ internal/utils/logger/logger_test.go
@@ -0,0 +1,92 @@
+package logger_test
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"log"
+	"os"
+	"path"
+	"runtime"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/alexdglover/sage/internal/utils/logger"
+)
+
+// This is a hack to move the working directory to the project root
+// meaning the test created the log file in (Project Root)/logs
+func init() {
+	_, filename, _, _ := runtime.Caller(0)
+	dir := path.Join(path.Dir(filename), "../../..") // change to suit test file location
+	err := os.Chdir(dir)
+	if err != nil {
+		panic(err)
+	}
+}
+
+type logMesaage struct {
+	Time  time.Time `json:"time"`
+	Level string    `json:"level"`
+	Msg   string    `json:"msg"`
+}
+
+func TestErrorLog(t *testing.T) {
+	buf := &bytes.Buffer{}
+
+	// Redirect STDOUT to a buffer
+	stdout := os.Stdout
+
+	r, w, err := os.Pipe()
+	if err != nil {
+		t.Errorf("Failed to redirect STDOUT")
+	}
+
+	os.Stdout = w
+
+	go func() {
+		scanner := bufio.NewScanner(r)
+		for scanner.Scan() {
+			buf.WriteString(scanner.Text())
+		}
+	}()
+
+	logger := logger.Get()
+	logger.Error("Error log")
+
+	// Reset output
+	w.Close()
+	os.Stdout = stdout
+
+	// Test output
+	t.Log(buf)
+	if buf.Len() == 0 {
+		t.Error("No information logged to STDOUT")
+	}
+	if strings.Count(buf.String(), "\n") > 1 {
+		t.Error("Expected only a single line of log output")
+	}
+
+	var logMessage logMesaage
+	err = json.Unmarshal(buf.Bytes(), &logMessage)
+	if err != nil {
+		t.Error("Error while decoding the log message", err.Error())
+	}
+
+	if logMessage.Level != "ERROR" {
+		t.Errorf("Log level should be ERROR got: %s", logMessage.Level)
+	}
+
+	if logMessage.Msg != "Error log" {
+		t.Errorf("Log message should be 'Error log' got: %s", logMessage.Msg)
+	}
+
+	//clean up log folder
+	t.Cleanup(func() {
+		cwd, _ := os.Getwd()
+		if err := os.RemoveAll(path.Join(cwd, "/logs")); err != nil {
+			log.Printf("ERROR: Removing test log folder: %v %s", err, cwd)
+		}
+	})
+}
